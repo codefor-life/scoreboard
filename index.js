f296143b802313e69e463ea79b507a6ee27bf81a
Chebolu Rohit <112311598+codefor-life@users.noreply.github.com>
2022-09-28 11:07:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let count = document.getElementById("color")
+let result=0
+function one(){
+    count.textContent = result+1
+    result = result+1
+}
+function two(){
+    count.textContent = result+2
+    result = result+2
+}
+
+function three(){
+    count.textContent = result+3
+    result = result+3
+}
+
+let count1 = document.getElementById("range")
+let res = 0
+function one1(){
+    count1.textContent = res+1
+    res = res+1
+}
+function two2(){
+    count1.textContent = res+2
+    res = res+2
+}
+
+function three3(){
+    count1.textContent = res+3
+    res = res+3
+}
